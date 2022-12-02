4b1135ff365a1d8654fcfeec67c4e6ef5bb76a09
Shivani21-hude <shivani21hude@gmail.com>
2022-12-02 17:56:04 +0530

    Program to print the number in reverse order

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to reverse");
+            int n=Convert.ToInt32(Console.ReadLine());
+            
+            int d=0;
+            
+            while(0<n)
+            {
+                int num =  n % 10;
+                d= (d * 10) + num;
+                n = n / 10;
+            }
+            Console.WriteLine("The number in reverse order: " + d);
+            Console.ReadLine();
+        }
+    }
+}
