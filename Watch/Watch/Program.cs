5e43baeefd50dc85986c33d9da31a2b7b5ca9843
Shivani21-hude <shivani21hude@gmail.com>
2022-12-02 18:01:41 +0530

    Problem of stopwatch

---
+++ Watch/Watch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for startwatch: ");
+             int Start=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Enter a number for endwatch: ");
+             int Stop = Convert.ToInt32(Console.ReadLine());
+             Stopwatch stopwatch = new Stopwatch();
+             stopwatch.Start();
+            stopwatch.Stop();
+             Console.WriteLine("Time Elapsed : {0}",
+             stopwatch.Elapsed);
+            
+        }
+    
+    }
+}
