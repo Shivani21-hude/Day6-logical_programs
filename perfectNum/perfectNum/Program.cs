c10401210bf8ec46276ddb7365940cb94548390d
Shivani21-hude <shivani21hude@gmail.com>
2022-12-02 21:06:59 +0530

    Program to print perfect number

---
+++ perfectNum/perfectNum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfectNum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1.PerfectNum();
+        }
+    
+    }
+}
