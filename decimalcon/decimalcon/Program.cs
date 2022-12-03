f6789a72b6271742c4990a5a8057e878ce2ec3f8
Shivani21-hude <shivani21hude@gmail.com>
2022-12-03 07:55:34 +0530

    Conversion of decimal to binary number

---
+++ decimalcon/decimalcon/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace decimalcon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Decimal Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int[] numberArray = new int[10];
+            for (i = 0; number > 0; i++)
+            {
+                numberArray[i] = number % 2;
+                number = number / 2;
+            }
+            Console.Write("Binary Represenation of the given Number : ");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(numberArray[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
