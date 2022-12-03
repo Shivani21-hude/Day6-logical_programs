7047e17ec5c38e92b7950ed9637b0f7f9b47ce09
Shivani21-hude <shivani21hude@gmail.com>
2022-12-03 09:20:39 +0530

    Program of Swap two nibbles in byte

---
+++ binaryfun/binaryfun/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryfun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Decimal Number : ");
+           int i = Convert.ToInt32(Console.ReadLine());
+
+            i =(((i&0x0F)<<4)|((i&0xF0)>>4));
+            
+            Console.Write("Swpping of two nibbles:"+ i);
+        }
+    }
+}
